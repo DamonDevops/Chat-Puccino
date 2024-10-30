ba9de99c98a37af98566130333f3ed3081a45634
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-10-30 13:30:25 +0100

    starting UI Design

--- ChatPuccino/App.xaml.cs
+++ ChatPuccino/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ChatPuccino
+﻿using ChatPuccino.Views;
+
+namespace ChatPuccino
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new LandingPage();
         }
     }
 }
